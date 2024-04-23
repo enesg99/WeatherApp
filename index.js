ba7b16350776c64a2b1a55c1a984327f10e36eca
Enes <enesgurleyen@hotmail.de>
2024-04-23 12:52:35 +0200

    Implementing general api function

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+const APIKey = "7dfd05918020b2464b04d38f736b4dbb";
+const input = document.getElementById("cityInput");
+
 window.addEventListener("keydown", (event) => {
     if(event.key == "Enter") {
         search()
     }
 })
 
-function search() {
-    alert("Funktioniert noch nicht lol")
-}
+async function search() {
+    try {
+        const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${APIKey}`
+        );
+        const data = await response.json();
+        console.log(data)
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+  async function city() {
+    try {
+        const response = await fetch(
+            "https://countriesnow.space/api/v0.1/countries"
+        )
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+  }
